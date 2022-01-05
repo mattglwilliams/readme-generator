933cf850e18f9f8e8f36a445041945c7be8e0a85
Matthew Williams <matthewwilliams@matts-mbp.home>
2022-01-05 17:41:31 +0000

    Linked the generate markdown function

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const questions = require('./questions')
-const utils = require('./utils')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const questions = require("./questions");
+const utils = require("./utils");
 
 async function init() {
-    const answers = await inquirer.prompt(questions)
-  
-    if (answers) {
-      const readmeContent = utils.generateHTML(answers);
-  
-      fs.writeFile('index.html', readmeContent, (err) =>
-        err ? console.log(err) : console.log('Successfully created index.html!')
-      );
-    } else {
-      console.error("Something went wrong")
-    }
+  const answers = await inquirer.prompt(questions);
+
+  if (answers) {
+    const readmeContent = utils.generateMarkdown(answers);
+
+    fs.writeFile("index.html", readmeContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created index.html!")
+    );
+  } else {
+    console.error("Something went wrong");
   }
-  
-  init();
+}
+
+init();
