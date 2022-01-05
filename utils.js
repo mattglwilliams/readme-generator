9727eeb35c95ff0dc016f1b870ef7c987439d157
Matthew Williams <matthewwilliams@matts-mbp.home>
2022-01-05 17:54:00 +0000

    Created the markdown template

---
+++ utils.js
@@ -0,0 +1,46 @@
+const generateMarkdown = ({
+  title,
+  description,
+  installation,
+  usage,
+  licenseBadge,
+  licenseDesc,
+  contribution,
+  testing,
+  github,
+  email,
+}) => `# ${title}
+(https://img.shields.io/badge/License-${licenseBadge}-green.svg)
+## Contents
+* [Description] (#description)
+* [Installation] (#installation)
+* [Usage] (#usage)
+* [License] (#license)
+* [Contribution] (#contribution)
+* [Testing] (#testing)
+* [Questions] (#questions)
+
+### Description
+${description}
+
+### Installation
+${installation}
+
+### Usage
+${usage}
+
+### License
+${licenseDesc}
+
+### Contribution
+${contribution}
+
+### Testing
+${testing}
+
+### Questions
+Please contact me at either of the below options:
+* https://github.com/${github}
+* ${email}`;
+
+module.exports = { generateMarkdown };
